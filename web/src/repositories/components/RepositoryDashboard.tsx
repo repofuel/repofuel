27252c943158770e34d1a618ab1c1c335f161dda
Suhaib Mujahid <suhaibmujahid@gmail.com>
2022-08-11 21:13:36 -0400

    chore(web): Drop the charts from the repository view

--- web/src/repositories/components/RepositoryDashboard.tsx
+++ web/src/repositories/components/RepositoryDashboard.tsx
@@ -1,12 +1,8 @@
-import React, {useEffect, useMemo, useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 import graphql from 'babel-plugin-relay/macro';
 
 import {RepositoryDashboardQuery} from './__generated__/RepositoryDashboardQuery.graphql';
-import {
-  useFragment,
-  useLazyLoadQuery,
-  useRefetchableFragment,
-} from 'react-relay/hooks';
+import {useLazyLoadQuery, useRefetchableFragment} from 'react-relay/hooks';
 import {CommitsList} from './CommitsList';
 import {GridCell, GridRow} from '@rmwc/grid';
 import {Typography} from '@rmwc/typography';
@@ -16,9 +12,7 @@ import {RiskyCommitsDoughnut} from './RiskyCommitsDoughnut';
 import './RepositoryDashboard.scss';
 
 import {Chart, ChartDataSets, TimeUnit} from 'chart.js';
-import {Tooltip} from '@rmwc/tooltip';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faQuestionCircle} from '@fortawesome/free-regular-svg-icons';
 import {RepositoryAddress} from '../types';
 import Masonry from 'react-masonry-css';
 import {Link, RouteComponentProps, useParams} from 'react-router-dom';
@@ -29,10 +23,6 @@ import {Flash} from '@primer/components';
 import {useStatusTracker} from './Progress';
 import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
 import {FormatNumber} from '../../util/number-formate';
-import {fillPointsOnTimeLine, generateTimeLine} from '../../util/chart';
-import {RepositoryDashboard_tagsCount$key} from './__generated__/RepositoryDashboard_tagsCount.graphql';
-import {RepositoryDashboard_commitsOverTime$key} from './__generated__/RepositoryDashboard_commitsOverTime.graphql';
-import {RepositoryDashboard_avgCommitFilesOverTime$key} from './__generated__/RepositoryDashboard_avgCommitFilesOverTime.graphql';
 import {PullRequestsList} from './PullRequestsList';
 
 const DASHBOARD_SCREEN_PAGE_SIZE = 5;
@@ -85,9 +75,6 @@ export const RepositoryDashboard: React.FC<RepositoryDashboardProps> = ({
             }
           }
         }
-        ...RepositoryDashboard_commitsOverTime
-        ...RepositoryDashboard_tagsCount
-        ...RepositoryDashboard_avgCommitFilesOverTime
         branchesCount
         buggyCommitsCount
         commitPredictionsCount
@@ -113,8 +100,6 @@ export const RepositoryDashboard: React.FC<RepositoryDashboardProps> = ({
     'READY',
     'WATCHED'
   );
-
-  const timeLine = generateTimeLine('YEAR', 'MONTHLY');
 
   return (
     <>
@@ -195,51 +180,6 @@ export const RepositoryDashboard: React.FC<RepositoryDashboardProps> = ({
             </CardActions>
           </CommitsList>
         </Card>
-
-        <Card outlined>
-          <div style={{margin: '15px'}}>
-            <Typography use="headline5" style={{marginBottom: '5px'}}>
-              Commits
-            </Typography>
-            <Tooltip
-              content="The chart shows the number of buggy commits over time in this repository."
-              showArrow
-              align={'bottom'}>
-              <FontAwesomeIcon
-                className="margin-left"
-                icon={faQuestionCircle}
-              />
-            </Tooltip>
-          </div>
-          <BuggyCommitsChart timeLine={timeLine} repository={repository} />
-        </Card>
-
-        <Card outlined>
-          <div style={{margin: '15px'}}>
-            <Typography use="headline5" style={{marginBottom: '5px'}}>
-              Activities
-            </Typography>
-          </div>
-          <TagsChart repository={repository} />
-        </Card>
-
-        <Card outlined>
-          <div style={{margin: '15px'}}>
-            <Typography use="headline5" style={{marginBottom: '5px'}}>
-              Change Size
-            </Typography>
-            <Tooltip
-              content="The chart shows the size of changes in commits over time in this repository."
-              showArrow
-              align={'bottom'}>
-              <FontAwesomeIcon
-                className="margin-left"
-                icon={faQuestionCircle}
-              />
-            </Tooltip>
-          </div>
-          <CommitFilesChart timeLine={timeLine} repository={repository} />
-        </Card>
       </Masonry>
     </>
   );
@@ -317,125 +257,6 @@ export const WidgetCard: React.FC<WidgetCardProps> = ({
   );
 };
 
-interface TagsChartProps {
-  repository: RepositoryDashboard_tagsCount$key;
-}
-
-const TagsChart: React.FC<TagsChartProps> = (props) => {
-  const {tagsCount} = useFragment(
-    graphql`
-      fragment RepositoryDashboard_tagsCount on Repository {
-        tagsCount {
-          nodes {
-            x: tag
-            y: count
-          }
-        }
-      }
-    `,
-    props.repository
-  );
-
-  const datasets = useMemo(
-    () => [
-      {
-        backgroundColor: 'rgba(81,66,129,0.8)',
-        data: tagsCount?.nodes?.map((v: any) => v.y),
-      },
-    ],
-    [tagsCount]
-  );
-
-  return (
-    <HorizontalBarChart
-      datasets={datasets}
-      labels={tagsCount?.nodes?.map((v: any) => v.x) || []}
-    />
-  );
-};
-
-interface BuggyCommitsChartProps {
-  repository: RepositoryDashboard_commitsOverTime$key;
-  timeLine: string[];
-}
-
-const BuggyCommitsChart: React.FC<BuggyCommitsChartProps> = (props) => {
-  const {buggyCommitsOverTime, commitsOverTime} = useFragment(
-    graphql`
-      fragment RepositoryDashboard_commitsOverTime on Repository {
-        buggyCommitsOverTime {
-          nodes {
-            x: date
-            y: count
-          }
-        }
-        commitsOverTime {
-          nodes {
-            x: date
-            y: count
-          }
-        }
-      }
-    `,
-    props.repository
-  );
-
-  const datasets = useMemo(
-    () => [
-      {
-        label: 'Buggy Commits',
-        data: fillPointsOnTimeLine(props.timeLine, buggyCommitsOverTime?.nodes),
-        backgroundColor: 'rgba(255,99,104,0.7)',
-      },
-      {
-        label: 'All Commits',
-        data: fillPointsOnTimeLine(props.timeLine, commitsOverTime?.nodes),
-        backgroundColor: 'rgba(54,162,235,0.35)',
-      },
-    ],
-    [buggyCommitsOverTime, commitsOverTime, props.timeLine]
-  );
-
-  return <LineChart unit="month" datasets={datasets} />;
-};
-
-interface CommitFilesChartProps {
-  repository: RepositoryDashboard_avgCommitFilesOverTime$key;
-  timeLine: string[];
-}
-
-const CommitFilesChart: React.FC<CommitFilesChartProps> = (props) => {
-  const {avgCommitFilesOverTime} = useFragment(
-    graphql`
-      fragment RepositoryDashboard_avgCommitFilesOverTime on Repository {
-        avgCommitFilesOverTime {
-          nodes {
-            x: date
-            y: avg
-          }
-        }
-      }
-    `,
-    props.repository
-  );
-
-  const datasets = useMemo(
-    () => [
-      {
-        label: 'Average number of files',
-        data: fillPointsOnTimeLine(
-          props.timeLine,
-          avgCommitFilesOverTime?.nodes
-        ),
-        backgroundColor: 'rgba(81,66,129,0.4)',
-      },
-    ],
-    [avgCommitFilesOverTime, props.timeLine]
-  );
-
-  return <LineChart unit="month" datasets={datasets} />;
-};
-
 interface LineChartProps {
   datasets: ChartDataSets[];
   unit: TimeUnit;
@@ -498,48 +319,6 @@ export const LineChart: React.FC<LineChartProps> = ({datasets, unit}) => {
       },
     });
   }, [datasets, unit]);
-
-  return <canvas ref={canvasRef} />;
-};
-
-interface HorizontalBarChartProps {
-  datasets: ChartDataSets[];
-  labels: string[];
-}
-
-export const HorizontalBarChart: React.FC<HorizontalBarChartProps> = ({
-  datasets,
-  labels,
-}) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const chartInstanceRef = useRef<Chart | null>(null);
-
-  useEffect(() => {
-    const chartInstance = chartInstanceRef.current;
-    if (chartInstance) {
-      chartInstance.data.labels = labels;
-      chartInstance.data.datasets = datasets;
-      chartInstance.update();
-      return;
-    }
-
-    if (!canvasRef.current) {
-      throw Error('canvasRef should be assigned to an HTMLCanvasElement');
-    }
-
-    chartInstanceRef.current = new Chart(canvasRef.current, {
-      type: 'horizontalBar',
-      data: {
-        labels,
-        datasets,
-      },
-      options: {
-        legend: {
-          display: false,
-        },
-      },
-    });
-  }, [labels, datasets]);
 
   return <canvas ref={canvasRef} />;
 };
